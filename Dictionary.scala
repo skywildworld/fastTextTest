8fcab6b8ff9439e34a3a3d3da9486ad2a909f19b
Ryan Xu <skywildworld@gmail.com>
2017-05-01 12:25:59 +0800

    dictionary modify

--- Dictionary.scala
+++ Dictionary.scala
@@ -1,68 +1,126 @@
-import scala.io.source
+package com.fastText
+
+import scala.io.Source
 import scala.collection.mutable.ArrayBuffer
 
 import java.io._
 
 
-type Int IdType
 sealed trait EntryType  {val value: Int}
 case object Word extends EntryType {val value = 0}
 case object Label extends EntryType {val value = 1}
 
-case class Entry(word:String, count:Long, type: EntryType, subwords: Array[Int])
+case class Entry(word:String, var count:Long, typeE: EntryType, var subwords: Array[Int])
 
 class Dictionary() {
-  var args: Args
+
+  private val MAX_VOCAB_SIZE = 30000000
+  private val MAX_LINE_SIZE = 1024
+  
+  private val EOS = "</s>"
+  private val BOW = "<"
+  private val EOW = ">"
+
+  type IdType = Int
+  var args: Args = _
   var size = 0
   var nwords = 0
   var nlabels = 0
   var ntokens = 0
   var word2Int = ArrayBuffer.fill[Int](MAX_VOCAB_SIZE)(-1)
-  var words: ArrayBuffer[Entry]
-  var pdiscard: ArrayBuffer[Double]
+  var words: ArrayBuffer[Entry] = _ 
+  var pdiscard: ArrayBuffer[Double] = _
 
-  def load(s: Source) {
-    words.clean() 
-    word2Int = ArrayBuffer.fill[Int](MAX_VOCAB_SIZE)(-1)
-
+  def add(w: String) {
+    val h = find(w)
+    ntokens += 1
+    if (word2int(h) == -1) {
+      val word = w
+      val count = 1
+      val typeE = if (w.contains(args_->label)) Label else Word 
+      words.append(Entry(word, count, typeE, Array[Int]()))
+    } else {
+      words(word2int(h)).count += 1
+    }
   }
-  def save(out: FileOutputStream) {
-      out.write(size)
-      out.write(nwords)
-      out.write(nlabels)
-      out.write(ntokens)
-      words.foreach( item => {
-        val Entry(word, count, type, subwords) = item
 
-      }
+  def nwords = nwords
+  def nlabels = nlabels
+  def ntokens = ntokens
+  
+  def getNgrams(i: Int): Array[Int] = {
+    require(i >= 0)
+    require(i < nwords)
+    words(i).subwords
+  }
+
+  def getNgrams(word: String): Array[Int] = {
+    val i = getId(word)
+    if (i >= 0) {
+      getNgrams(i)
+    } else {
+      
+      
+    }
+  }
+
+  def getId(w: String) : Int = {
+    val h = find(w)
+    word2int(h)
+  }
+
+  def load(in: DataInputStream) {
+    words.clear() 
+    word2Int = ArrayBuffer.fill[Int](MAX_VOCAB_SIZE)(-1)
+    size = in.readInt()
+    nwords = in.readInt()
+    nlabels = in.readInt()
+    ntokens = in.readInt()
+    for (index <- 0 until size)  {
+      val word = in.readUTF()
+      in.readInt()
+      val count = in.readInt()
+      val typeValue = in.readInt() 
+      val typeE = if (typeValue == 0) Word else Label 
+      words.append(Entry(word, count, typeE, Array[Int]()))
+      word2Int(find(word)) = index
+    } 
+  }
+  def save(out: DataOutputStream) {
+      out.writeInt(size)
+      out.writeInt(nwords)
+      out.writeInt(nlabels)
+      out.writeInt(ntokens)
+      words.foreach(item => {
+        val Entry(word, count, typeE, subwords) = item
+        out.writeBytes(word)
+        out.write(0) 
+        out.writeLong(count)
+        out.writeInt(typeE.value)
+      })
   }
   
 
   private def find(w: String): Int = {
-    var h = hash(w)  % MAX_VOCAB_SIZE      
-    while(word2Int(h) != -1 && words
+    var h = (hash(w)  % MAX_VOCAB_SIZE).toInt
+    while(word2Int(h) != -1 && words(word2Int(h)).word != w) {
+      h = (h + 1) % MAX_VOCAB_SIZE
+    }
+    h.toInt
   }
-  private def hash(w: String) : Int = {
-    var h = 2166136261;
-    for (int i = 0; i < w.length; ++i) {
+  private def hash(w: String) : Long = {
+    var h = 2166136261L
+    for (i <- 0 until w.length) {
       h = h ^ w(i).toInt
       h = h * 16777619
     }
     h
   }
   
-  
-
-
 }
 
-
 object Dictionary {
-  private[this] val MAX_VOCAB_SIZE = 30000000
-  private[this] val MAX_LINE_SIZE = 1024;
-  
-  val EOS = "</s>"
-  val BOW = "<"
-  val EOW = ">"
+  def main(args: Array[String]) : Unit  {
+  }  
 
 }
